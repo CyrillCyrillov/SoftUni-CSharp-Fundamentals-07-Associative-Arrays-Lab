f61f6f8d602920625b06ea90caa8ad7fc0465ba6
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-19 23:25:20 +0200

    Add files via upload

---
+++ Task04_Largest_3_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Task03_Largest_3_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbersToSort = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            numbersToSort = numbersToSort.OrderByDescending(x => x).ToArray();
+
+            if(numbersToSort.Length >= 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{numbersToSort[i]} ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(' ', numbersToSort));
+            }
+        }
+    }
+}
