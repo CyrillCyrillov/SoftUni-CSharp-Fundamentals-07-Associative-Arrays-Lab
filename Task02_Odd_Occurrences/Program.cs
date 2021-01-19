2488f6ce0d9e0758e82c7442bb51ebe518ade06b
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-19 23:23:30 +0200

    Add files via upload

---
+++ Task02_Odd_Occurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _02_Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Dictionary<string, int> countWords = new Dictionary<string, int>();
+
+            foreach (string nextWord in words)
+            {
+                string lowCaseNextWor = nextWord.ToLower();
+                if(countWords.ContainsKey(lowCaseNextWor))
+                {
+                    countWords[lowCaseNextWor]++;
+                }
+                else
+                {
+                    countWords.Add(lowCaseNextWor, 1);
+                }
+            }
+
+            foreach (var keyWord in countWords)
+            {
+                if(keyWord.Value % 2 != 0)
+                {
+                    Console.Write($"{keyWord.Key} ");
+                }
+            }
+        }
+    }
+}
