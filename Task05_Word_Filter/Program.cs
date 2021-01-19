5bfbd2169ac5ef93a7af3dda95f554ac04aebc8d
Kiril Kirilov <kirilkirilov@abv.bg>
2021-01-19 23:26:15 +0200

    Add files via upload

---
+++ Task05_Word_Filter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Task05_Word_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] wordsToSelect = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+
+            string[] filteredWords = wordsToSelect.Where(x => x.Length % 2 == 0).ToArray();
+            
+            Console.WriteLine(string.Join((char)10, filteredWords));
+        }
+    }
+}
